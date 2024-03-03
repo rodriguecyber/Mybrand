444144ba99cde828ab6ef3d588aa1db2ae1dbd68
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-03-03 02:14:10 +0200

    working on responsiveness

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,19 @@
 let goToSkills=()=>{
-  window.scrollTo(1,351) 
+  window.scrollTo(1,1350) 
 }
 let goToHome=()=>{
   window.scrollTo(1,1)
 }
 let goToAbout=()=>{
-  window.scrollTo(1,840)
+  window.scrollTo(1,715)
 }
 let goToPortifolio=()=>{
-  window.scrollTo(1,1400)
+  window.scrollTo(1,1950)
 }
 let goToContact=()=>{
-  window.scrollTo(1,1950)
+ window.scrollTo(1,2470)
+}
+let goToBlog=()=>{
+  window.scrollTo(1,3000)
 }
 
