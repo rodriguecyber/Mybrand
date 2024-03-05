3ae5c8f799e77e8dc7ce60db09487606bd9c4bbf
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-03-05 16:57:10 +0200

    responsiveness

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,12 @@
-let showMessage = (n) =>{
-    document.getElementById(n).style.display="block"
+let openMessage = () =>{
+    document.getElementById('mess').style.display="block"
 }
-let hideMessage = (n) =>{
-    document.getElementById(n).style.display="none"
+let closeMessage = (message_id) =>{
+    document.getElementById("mess").style.display="none"
+}
+let showMessage = () =>{
+    document.getElementById('mess-box').style.display="block"
+}
+let hideMessage = () =>{
+    document.getElementById("mess-box").style.display="none"
 }
