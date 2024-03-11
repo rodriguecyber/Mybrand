6f80ba762b38e21b2564b8cc1c7118ca34080a64
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-03-11 14:00:24 +0200

    message functions

--- public/js/getMessage.js
+++ public/js/getMessage.js
@@ -4,18 +4,22 @@ let selectMessage = () => {
     var messages = JSON.parse(localStorage.getItem('message')) || [];
 
     messages.forEach((message, index) => {
-        let messagediv = templateDiv.cloneNode(true);
-        var messaged = messagediv.querySelector(".sender h4");
-        var emailed= messagediv.querySelector(".sender p");
-        messaged.textContent = message.names; 
-        emailed.textContent = message.address; 
-
         if (index === 0) {
             var originalMessagediv = templateDiv.querySelector(".sender h4");
             var originalAddressdiv = templateDiv.querySelector(".sender p");
             originalMessagediv.textContent = message.names;
             originalAddressdiv.textContent = message.address;
         }
+        else 
+       { 
+        let messagediv = templateDiv.cloneNode(true);
+        var messaged = messagediv.querySelector(".sender h4");
+        var emailed= messagediv.querySelector(".sender p");
+        messaged.textContent = message.names; 
+        emailed.textContent = message.address; 
+    }
+
+        
 
         // Append the cloned messagediv to the container
         divContainer.appendChild(messagediv);
