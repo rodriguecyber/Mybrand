fee90ffde133e9939c8f132a5b4843074b1be527
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-04-05 19:29:35 +0200

    fetching blogs from api

--- public/js/blogs.js
+++ public/js/blogs.js
@@ -1,84 +1,41 @@
- function displayBlogs() {
-    const blogsContainer = document.getElementById('blogs-container');
+ const displayBlogs=() =>{
+    const blogsContainer = document.getElementById('blogs-container');   
+    
+    blogsContainer.innerHTML = ''
+    try {
+        fetch('https://portfolio-back-end-1-pm2e.onrender.com/brand/blogs')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                
+                return response.json();
 
-   
-    function renderBlogs() {
-        blogsContainer.innerHTML = '';
-        const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
-        blogs.forEach((blog, index)  => {
-           if(index>=blogs.length-3){
-            const blogElement = document.createElement('div');
+            }).then(blogs=>{
+             blogs.forEach(blog => {
+                const blogElement=document.createElement('div')
             blogElement.classList.add('blog');
             blogElement.innerHTML = `
-                <h2>${blog.title}</h2>
-                <div class="image">
-                <img class="blog-image" src="${blog.image}" alt="Blog Image">
-                </div>
-                <p>${blog.content}</p>
-                <button class="like-btn" data-id="${blog.id}">Like</button>
-                <textarea class="comment-input" placeholder="Add a comment"></textarea>
-                <button class="comment-btn" data-id="${blog.id}">Comment</button>
-                <div class="comments" data-id="${blog.id}"></div>
-                <div class="likes-count">Likes: <span>${blog.likes}</span></div>
-            `;
-            blogsContainer.appendChild(blogElement);
+                    <h2>${blog.title}</h2>            
+                    <div class="image">
+                         <img class="blog-image" src="${blog.image}" alt="Blog Image">
+                    </div>
+                    <p>${blog.content}</p>
+                    <button class="like-btn" data-id="${blog._id}">Like</button>
+                    <textarea class="comment-input" placeholder="Add a comment"></textarea>
+                    <button class="comment-btn" data-id="${blog._id}">Comment</button>
+                    <div class="comments" data-id="${blog._id}"></div>
+                    <div class="likes-count">Likes: <span>${blog.likes}</span></div>
+            `   
+          blogsContainer.appendChild(blogElement); 
+             });
 
-            const likeBtn = blogElement.querySelector('.like-btn');
-            likeBtn.addEventListener('click', handleLike);
-
-            const commentBtn = blogElement.querySelector('.comment-btn');
-            commentBtn.addEventListener('click', handleComment);
-
-            
-            const savedComments = JSON.parse(localStorage.getItem(`blog_${blog.id}_comments`));
-            const commentsContainer = blogElement.querySelector('.comments');
-            if (savedComments) {
-                savedComments.forEach(comment => {
-                    const commentElement = document.createElement('p');
-                    commentElement.textContent = comment;
-                    commentsContainer.appendChild(commentElement);
-                });
-            }
-           }
-        });
-    }
-
-   
-    function handleLike(event) {
-        const blogId = event.target.getAttribute('data-id');
-        const blogs = JSON.parse(localStorage.getItem('blogs')) || [];
-        const blogIndex = blogs.findIndex(blog => blog.id == blogId);
-        if (blogIndex !== -1) {
-            blogs[blogIndex].likes = (blogs[blogIndex].likes || 0) + 1;
-            localStorage.setItem('blogs', JSON.stringify(blogs));
-            renderBlogs();
-        }
-    }
-
-   
-    function handleComment(event) {
-        const blogId = event.target.getAttribute('data-id');
-        const commentInput = event.target.parentElement.querySelector('.comment-input');
-        const commentText = commentInput.value.trim();
-        if (commentText !== '') {
-            const commentsContainer = event.target.parentElement.querySelector('.comments');
-            const commentElement = document.createElement('p');
-            commentElement.textContent = commentText;
-            commentsContainer.appendChild(commentElement);
-            saveComment(blogId, commentText);
-            commentInput.value = '';
-        } else {
-            alert('Please enter a valid comment.');
-        }
-    }
-
-    
-    function saveComment(blogId, comment) {
-        const savedComments = JSON.parse(localStorage.getItem(`blog_${blogId}_comments`)) || [];
-        savedComments.push(comment);
-        localStorage.setItem(`blog_${blogId}_comments`, JSON.stringify(savedComments));
-    }
-
-   
-    renderBlogs();
+            })
+            .catch(error => {
+               console.log(error);
+            });
+    } catch (error) {
+        alert(error.message);
+    }     
 }
+       
