93e0245fddf63fec5e1581addad6456c9a268a4a
RWIGARA Rodrigue <rodrirwigara@gmail.com>
2024-04-25 02:27:21 +0200

    protect admin router

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -1,50 +1,41 @@
-// let selectUser=async()=>{
-//   event.preventDefault()
-//   const alert=document.createElement('p')
-//   alert.classList.add('alert')
-//     username=document.getElementById('emaill').value
-//     password=document.getElementById('passwordl').value
-//     const user={
-//       email:username,
-//       password:password
-//     }
-//     try{
-//       await fetch('https://portfolio-back-end-1-pm2e.onrender.com/brand/login',{
-//     method:'POST',
-//      headers:{
-//       'Content-Type':'application/json',
-      
-//      },
-//      body:JSON.stringify(user)
+const getLoginToken = () => {
+    const name = 'loginToken=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
     
-//    })
-//    .then(response=>response.json())
-//    .then(async(result)=>{
-//      console.log(result)
-//     if(result.message==='Password does not match'){
-//       document.getElementById('emailvalid').innerText=result.message
-//       document.getElementById('emailvalid').style.visibility='visible'
-//     }
-//     else if(result.message=='Logged in'){
-//       storeMyToken(result.token)
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    
+    return ''; 
+}
 
-//     }
-//     else{
-//       document.getElementById('emailvalid').style.visibility='visible'
-//     }
-//    })
-//    .catch(error=>{
-//     window.alert('failed to login')
-//    })
 
-//   }
-//   catch(error){
-//     console.log(error)
-//   }
-   
-//     }
-//      const storeMyToken=async(token)=>{
-//       document.cookie=`loginToken=${token}; path=/`
-//        window.location='/admin/admin.html'
-
-//      }
+const isLoggedIn=()=>{
+    try{
+    fetch('https://portfolio-back-end-1-pm2e.onrender.com/brand/isloggedin',{
+        method:'post',
+        headers:{
+            'Content-Type':'application/json',
+            'Authorization': `Bearer ${getLoginToken()}`
+            }
+    })
+    .then(response=>{
+        response.json()
+        .then(res=>{
+            if(response.status!=200){
+                window.location='../index.html' 
+            }
+        })
+    })
+}
+catch(error){
+    console.log(error)
+}
+}
